9076ef63d8ae27cd85a711b7a5f4b1b67dfdf59a
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-24 16:17:57 +0200

    workingprogress palincroma

--- sneak 8 javascript palincroma/main.js
+++ sneak 8 javascript palincroma/main.js
@@ -1,12 +1,25 @@
 // JSnack 8:Scrivere una funzione per capire se una parola è palindroma. Quindi chiedere una parola all'utente, utilizzare la funzione per sapere se la parola inserita è palindroma e stampare un messaggio appropriato
 // Creo una funzione per capire se una parola è palindroma.
+function palincroma(parola) {
+    var array = parola.split('');
 
+}
 // Chiedo all'utente una parola
-
-// verifico che sia una parola e non un numero o una frase altrimenti richiedo l'inserimento
-
+var parola = prompt('Inserisci una parola: ', 'anna' )
 // visualizzo tale parola
-
+console.log('la parola inserita è: ' + parola);
 // valuto se la parola è palincroma
+if (palincroma()) {
+    // Stampo un messaggio con il risultato ottenuto
+    console.log('la parola inserita è: palincroma');
+} else {
+    // Stampo un messaggio con il risultato ottenuto
+    console.log('la parola inserita non è: palincroma');
+}
 
-// Stampo un messaggio con il risultato ottenuto
+
+// Brainstorming
+// split--> array
+// reverse sull array
+// if palora inserita= array
+// join sul l array
