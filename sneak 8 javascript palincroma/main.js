b9cc03d65c96897efa7ac31b59f57d985fccd8c4
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-24 16:33:38 +0200

    fixed commenti e join (' ') esercizio finito

--- sneak 8 javascript palincroma/main.js
+++ sneak 8 javascript palincroma/main.js
@@ -2,24 +2,29 @@
 // Creo una funzione per capire se una parola è palindroma.
 function palincroma(parola) {
     var array = parola.split('');
-
+    var controllo_palincroma = array.reverse();
+    var controllo2_palincroma = controllo_palincroma.join('');
+    if (controllo2_palincroma == parola) {
+        return true; // la parola è palincroma
+    }
+    else {
+        return false; // la parola  non è palincroma
+    }
 }
 // Chiedo all'utente una parola
-var parola = prompt('Inserisci una parola: ', 'anna' )
+var parola = prompt('Inserisc.i una parola: ', 'anna' )
 // visualizzo tale parola
 console.log('la parola inserita è: ' + parola);
 // valuto se la parola è palincroma
-if (palincroma()) {
-    // Stampo un messaggio con il risultato ottenuto
+if (palincroma(parola)) {
+    // Stampo un messaggio con il risultato ottenuto se è palincroma
     console.log('la parola inserita è: palincroma');
 } else {
-    // Stampo un messaggio con il risultato ottenuto
+    // Stampo un messaggio con il risultato ottenuto se  non è palincroma
     console.log('la parola inserita non è: palincroma');
 }
-
-
 // Brainstorming
 // split--> array
 // reverse sull array
-// if palora inserita= array
 // join sul l array
+// if palora inserita= array
