3bb8c6a91c9cc1d7a7eee0d9029eaef1339b0b9c
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-24 15:50:42 +0200

    schema istruzioni da fare passo a passo per l'esercizio palincroma

--- sneak 8 javascript palincroma/main.js
+++ sneak 8 javascript palincroma/main.js
@@ -1,22 +1,12 @@
-// JSnack 3: Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
-// chiedo i 5 numeri all utente
-// var num0 = parseInt(prompt('Scrivi il primo numero: ' ,0));
-// var num1 = parseInt(prompt('Scrivi il secondo numero: ' ,1));
-// var num2 = parseInt(prompt('Scrivi il terzo numero: ' ,2));
-// var num3 = parseInt(prompt('Scrivi il quarto numero: ' ,3));
-// var num4 = parseInt(prompt('Scrivi il quinto numero: ' ,4));
-// stampa la somma di tutti i numeri inseriti
-// console.log( num0 + num1 + num2 + num3 + num4);
+// JSnack 8:Scrivere una funzione per capire se una parola è palindroma. Quindi chiedere una parola all'utente, utilizzare la funzione per sapere se la parola inserita è palindroma e stampare un messaggio appropriato
+// Creo una funzione per capire se una parola è palindroma.
 
-// dichiaro inizzializzando la variabile somma a 0 perche 0 è l ememento neutro dell operazione somma
-var somma = 0;
-for (var i = 0; i < 5; i++) {
-    // chiedo all utente di inserire per 5 volte dei numeri
-    var num = parseInt(prompt('Scrivi un numero: '));
-    // stampa ogni singolo numero scritto dall utente
-    console.log(num);
-    // eseguo la somma per 5 volte per ogni numero inserito dall utente e salvo il valore di somma ad ogni singolo addendo
-    somma = somma + num;
-}
-// stampa la somma di tutti i numeri inseriti
-console.log(somma);
+// Chiedo all'utente una parola
+
+// verifico che sia una parola e non un numero o una frase altrimenti richiedo l'inserimento
+
+// visualizzo tale parola
+
+// valuto se la parola è palincroma
+
+// Stampo un messaggio con il risultato ottenuto
