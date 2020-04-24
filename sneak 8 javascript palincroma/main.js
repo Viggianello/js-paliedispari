a557ca21613a1b8e5570c65191ece84193b1b756
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-24 16:47:26 +0200

    aggiungi commenti esplicativi

--- sneak 8 javascript palincroma/main.js
+++ sneak 8 javascript palincroma/main.js
@@ -1,9 +1,13 @@
 // JSnack 8:Scrivere una funzione per capire se una parola è palindroma. Quindi chiedere una parola all'utente, utilizzare la funzione per sapere se la parola inserita è palindroma e stampare un messaggio appropriato
 // Creo una funzione per capire se una parola è palindroma.
 function palincroma(parola) {
+    // trasformo la parola, stringa,inserita in un array (tramite la funzione .split()) con ogni lettera di tale parola separate da uno spazio(xke lo messo io se no di default separa gli elementi con ,).
     var array = parola.split('');
+    //applico la funzione .reverse(), la quale può essere applicata solo ad un array, che ha creato nella riga soprastante,in modo tale da invertire il suo contenuto (nota: ho ancora gli spazi tra ogni singola lettera).
     var controllo_palincroma = array.reverse();
+    //applico la funzione .join(), la quale può essere applicata solo ad un array, in modo tale da unire tutti gli elementi di tale array generando una stringa, levando gli spazi tra ogni singola lettera.
     var controllo2_palincroma = controllo_palincroma.join('');
+    // controllo se la parola invertita è uguale a quella non invertita.
     if (controllo2_palincroma == parola) {
         return true; // la parola è palincroma
     }
